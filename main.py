0bf0723f36e40f4ab892cfbd6216747b00a0131a
higor2100 <42756331+higor2100@users.noreply.github.com>
2021-09-23 21:12:13 -0300

    Atualização da base de dados
    Formação de linhas
    Organização do arquivo

--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-tabelaUpload = pd.read_csv("PACS01 - Trafego Out VDX PACS01_To_CORE02.csv", sep=";")[::-1]
-tabelaDownload = pd.read_csv("PACS01 - Trafego In VDX PACS01_To_CORE02.csv", sep=";")[::-1]
-print(tabelaUpload)
-print("**********************************************************************")
-print(tabelaDownload)
--- manipulacao_arquivos.py
+++ manipulacao_arquivos.py
@@ -1,3 +1,5 @@
+from numpy.core.fromnumeric import partition
+import pandas as pd
 #Primeiro arquivo
 file = open("arquivo1.html")
 fileRead = file.read().replace("","")
@@ -5,10 +7,17 @@ file.close()
 
 nomeArquivo = fileRead[fileRead.find("<span>")+len("<span>"):fileRead.find("</span>")]
 tabelaPreConversao = fileRead[fileRead.find("<pre>")+len("<pre>"):fileRead.find("</pre>")]
+conv = (tabelaPreConversao.replace("<br>","\n")).replace(" ", ";")
 
-file = open(nomeArquivo.replace(":"," -")+".csv","w+")
-file.write("Data;Hora;NDeSerie;QtdBites\n"+(tabelaPreConversao.replace("<br>","\n")).replace(" ", ";"))
-file.close
+tabelaUpload = pd.DataFrame([x.split(';') for x in conv.split('\n')],columns=['Data','Hora','NDeSerie','QtdBites'])
+tabelaUpload.insert(0,"Data Hora",tabelaUpload['Data'] + " " + tabelaUpload['Hora'])
+
+tabelaUpload['Data Hora'] = pd.to_datetime(tabelaUpload['Data Hora'], format='%d-%m-%Y %H:%M:%S').dt.strftime('%Y-%m-%d %H:%M:%S')
+del tabelaUpload['NDeSerie'],tabelaUpload['Data'],tabelaUpload['Hora']
+tabelaUpload = tabelaUpload[:-1]
+
+tabelaUpload[::-1].to_csv(r"./"+nomeArquivo.replace(":"," -")+".csv",index = False, header=True)
+
 
 #Segundo arquivo 
 file = open("arquivo2.html")
@@ -17,7 +26,13 @@ file.close()
 
 nomeArquivo = fileRead[fileRead.find("<span>")+len("<span>"):fileRead.find("</span>")]
 tabelaPreConversao = fileRead[fileRead.find("<pre>")+len("<pre>"):fileRead.find("</pre>")]
+conv = (tabelaPreConversao.replace("<br>","\n")).replace(" ", ";")
 
-file = open(nomeArquivo.replace(":"," -")+".csv","w+")
-file.write("Data;Hora;NDeSerie;QtdBites\n"+(tabelaPreConversao.replace("<br>","\n")).replace(" ", ";"))
-file.close
+tabelaDownload = pd.DataFrame([x.split(';') for x in conv.split('\n')],columns=['Data','Hora','NDeSerie','QtdBites'])
+tabelaDownload.insert(0,"Data Hora",tabelaDownload['Data'] + " " + tabelaDownload['Hora'])
+
+tabelaDownload['Data Hora'] = pd.to_datetime(tabelaDownload['Data Hora'], format='%d-%m-%Y %H:%M:%S').dt.strftime('%Y-%m-%d %H:%M:%S')
+del tabelaDownload['NDeSerie'],tabelaDownload['Data'],tabelaDownload['Hora']
+tabelaDownload = tabelaDownload[:-1]
+
+tabelaDownload[::-1].to_csv(r"./"+nomeArquivo.replace(":"," -")+".csv",index = False, header=True)
