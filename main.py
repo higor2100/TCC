5dfd34fbf3b3b32383933d397f72960df1a081f1
higor2100 <42756331+higor2100@users.noreply.github.com>
2021-09-30 13:34:10 -0300

    Atualização e organização
    Agora é possivel criar duas versões do jupyter

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+execfile('manipulacao_arquivos.py')
+path = "./Jupyter/Arquivo CSV/"
+files = os.listdir(path)
+def jupyterFile(f):    
+    abrir = open("modelo")
+    if f.find("In"):
+        abrirLeitura = abrir.read().replace("download","upload").replace("REPORLOCAL","./Arquivo CSV/"+f)
+    else:
+        abrirLeitura = abrir.read().replace("REPORLOCAL","./Arquivo CSV/"+f)
+    abrir.close()
+
+    salvar = open("./Jupyter/"+f[0:f.find(".csv")]+".ipynb","w")
+    salvar.write(abrirLeitura)
+    salvar.close()
+
+for f in files:
+    jupyterFile(f)
+
--- manipulacao_arquivos.py
+++ manipulacao_arquivos.py
@@ -1,56 +1,40 @@
 import pandas as pd
+import os
 
-#Primeiro arquivo
-#1 - Abertura do arquivo em HTML
-file = open("arquivo1.html")
-fileRead = file.read().replace("","")
-file.close()
+path = "./Arquivo HTML/"
+files = os.listdir(path)
 
-#2 - Manipulação do arquivo em HTML
-nomeArquivo = fileRead[fileRead.find("<span>")+len("<span>"):fileRead.find("</span>")]
-tabelaPreConversao = fileRead[fileRead.find("<pre>")+len("<pre>"):fileRead.find("</pre>")]
-conv = (tabelaPreConversao.replace("<br>","\n")).replace(" ", ";")
+def manipular(file):
+    #1 - Abertura do arquivo em HTML
+    abrir = open(path+f)
+    abrirLeitura = abrir.read().replace("","")
+    abrir.close()
 
-#3 - Criação da Base de dados
-tabelaUpload = pd.DataFrame([x.split(';') for x in conv.split('\n')],columns=['Data','Hora','NDeSerie','download'])
-tabelaUpload.insert(0,"time",tabelaUpload['Data'] + " " + tabelaUpload['Hora'])
+    #2 - Manipulação do arquivo em HTML
+    nomeArquivo = abrirLeitura[abrirLeitura.find("<span>")+len("<span>"):abrirLeitura.find("</span>")]
+    tabelaPreConversao = abrirLeitura[abrirLeitura.find("<pre>")+len("<pre>"):abrirLeitura.find("</pre>")]
+    conv = (tabelaPreConversao.replace("<br>","\n")).replace(" ", ";")
 
-#4 - Formatação dos dados 
-tabelaUpload['time'] = pd.to_datetime(tabelaUpload['time'], format='%d-%m-%Y %H:%M:%S').dt.strftime('%Y-%m-%d %H:%M:%S')
-tabelaUpload['download'] = pd.to_numeric(tabelaUpload['download'])
+    #3 - Criação da Base de dados
+    if nomeArquivo.find("In"):
+        tabelaUpload = pd.DataFrame([x.split(';') for x in conv.split('\n')],columns=['Data','Hora','NDeSerie','upload'])
+    else:
+        tabelaUpload = pd.DataFrame([x.split(';') for x in conv.split('\n')],columns=['Data','Hora','NDeSerie','download'])
+    tabelaUpload.insert(0,"time",tabelaUpload['Data'] + " " + tabelaUpload['Hora'])
 
-#5 - Limpeza dos dados desnecessarios
-del tabelaUpload['NDeSerie'],tabelaUpload['Data'],tabelaUpload['Hora']
-tabelaUpload = tabelaUpload[:-1]
+    #4 - Limpeza dos dados desnecessarios
+    del tabelaUpload['NDeSerie'],tabelaUpload['Data'],tabelaUpload['Hora']
+    tabelaUpload = tabelaUpload[:-1]
 
-#6 - Salvamento do arquivo em formato CSV
-tabelaUpload[::-1].to_csv(r"./"+nomeArquivo.replace(":"," -")+".csv",index = False, header=True)
+    #5 - Formatação dos dados 
+    tabelaUpload['time'] = pd.to_datetime(tabelaUpload['time'], format='%d-%m-%Y %H:%M:%S').dt.strftime('%Y-%m-%d %H:%M:%S')
+    if nomeArquivo.find("In"):
+        tabelaUpload['upload'] = tabelaUpload['upload'].astype(object).astype(int)
+    else:
+        tabelaUpload['download'] = tabelaUpload['download'].astype(object).astype(int)
+    
+    #6 - Salvamento do arquivo em formato CSV
+    tabelaUpload[::-1].to_csv(r"./Jupyter/Arquivo CSV/"+nomeArquivo.replace(":"," -")+".csv",index = False, header=True)
 
-##################################################################################################################################
-
-#Segundo arquivo 
-#Repete o mesmo passo acima
-file = open("arquivo2.html")
-fileRead = file.read().replace("","")
-file.close()
-
-nomeArquivo = fileRead[fileRead.find("<span>")+len("<span>"):fileRead.find("</span>")]
-tabelaPreConversao = fileRead[fileRead.find("<pre>")+len("<pre>"):fileRead.find("</pre>")]
-conv = (tabelaPreConversao.replace("<br>","\n")).replace(" ", ";")
-
-tabelaDownload = pd.DataFrame([x.split(';') for x in conv.split('\n')],columns=['Data','Hora','NDeSerie','download'])
-tabelaDownload.insert(0,"time",tabelaDownload['Data'] + " " + tabelaDownload['Hora'])
-
-tabelaDownload['time'] = pd.to_datetime(tabelaDownload['time'], format='%d-%m-%Y %H:%M:%S').dt.strftime('%Y-%m-%d %H:%M:%S')
-tabelaDownload['download'] = pd.to_numeric(tabelaDownload['download'])
-
-del tabelaDownload['NDeSerie'],tabelaDownload['Data'],tabelaDownload['Hora']
-tabelaDownload = tabelaDownload[:-1]
-
-tabelaDownload[::-1].to_csv(r"./"+nomeArquivo.replace(":"," -")+".csv",index = False, header=True)
-
-##################################################################################################################################
-#Salvamento em XLSX para analise de Dados com Excel
-with pd.ExcelWriter('UnirEmExcel.xlsx') as writer:
-    tabelaUpload.to_excel(writer,sheet_name="Upload",index=False)
-    tabelaDownload.to_excel(writer,sheet_name="Download",index=False)
+for f in files:
+    manipular(f)
